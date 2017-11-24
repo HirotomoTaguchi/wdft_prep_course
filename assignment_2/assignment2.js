feffaf9f051940966e04e90ab0a84d71ff767350
Hirotomo Taguchi <kkhat67@gmail.com>
2017-11-24 10:07:16 -0800

    update

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -8,11 +8,11 @@ Write your explanation for Exercise #1 here
 */
 for (i=0; i<101; i++;){
     if (i/15 == 0){
-        console.log(fizzbuzz);
+        console.log("fizzbuzz");
     }else if(i/3==0){
-        console.log(buzz);
+        console.log("buzz");
     }else if(i/5==0){
-        console.log(fizz);
+        console.log("fizz");
     }else{
         console(i);
     }
