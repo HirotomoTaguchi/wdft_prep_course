224a8b02e51ca9d2d2019dce429b7421619ca7f4
Hirotomo Taguchi <kkhat67@gmail.com>
2017-11-24 10:00:41 -0800

    test

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -6,10 +6,17 @@
 Write your explanation for Exercise #1 here
 
 */
-
-
-
-
+for (i=0; i<101; i++;){
+    if (i/15 == 0){
+        console.log(fizzbuzz);
+    }else if(i/3==0){
+        console.log(buzz);
+    }else if(i/5==0){
+        console.log(fizz);
+    }else{
+        console(i);
+    }
+}
 
 // Exercise #2
 // Write your solution below
